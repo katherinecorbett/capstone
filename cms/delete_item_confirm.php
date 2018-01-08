ef7e84fc49b7ff22325112727107d7fd53c97b4b
katherinecorbett <35071452+katherinecorbett@users.noreply.github.com>
2018-01-08 12:05:26 -0400

    Update delete_item_confirm.php
    escape vars in sql

--- cms/delete_item_confirm.php
+++ cms/delete_item_confirm.php
@@ -23,6 +23,7 @@
 
     $TableName = "items";
 	$QueryString = "SELECT * FROM $TableName WHERE item_id='$item_id'";
+	$QueryString = mysqli_real_escape_string($QueryString);
 	$QueryResult = mysqli_query($conn, $QueryString) or trigger_error( 
 						mysqli_error(), E_USER_ERROR);
 	while ($Row = mysqli_fetch_assoc($QueryResult)) {
