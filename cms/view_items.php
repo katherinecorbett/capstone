c37c3b8b4cb5e9ccbf44c688329b8bab07be4f23
katherinecorbett <35071452+katherinecorbett@users.noreply.github.com>
2018-01-08 12:15:11 -0400

    Update view_items.php
    escape vars in sql

--- cms/view_items.php
+++ cms/view_items.php
@@ -19,6 +19,8 @@
 
  	$TableName = "items";
 	$QueryString = "SELECT * FROM $TableName ORDER BY description ASC";
+	$QueryString = mysqli_real_escape_string($QueryString);
+	
 	$QueryResult = mysqli_query($conn, $QueryString) or trigger_error( 
 						mysqli_error(), E_USER_ERROR);
 	while ($Row = mysqli_fetch_assoc($QueryResult)) {
